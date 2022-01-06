5d3f95320523d946a5675d845d6b2458aa5a1a02
Loktev Alexey <arloktev@mail.ru>
2022-01-06 07:11:44 +0300

    refactor(cli): рефакторинг на async/await, правки по код ревью

---
+++ src/utils.js
@@ -0,0 +1,32 @@
+'use strict';
+
+class Utils {
+  /**
+   * Получение случайного целого числа от min до max
+   * @param {number} min Минимальное число
+   * @param {number} max Максимальное число
+   * @return {number}
+   */
+  static getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+  /**
+   * Перемешивает (переупорядочивает случайным образом) элементы массива (Тасование Фишера — Йетса)
+   * @param {*} array
+   * @return {*}
+   */
+  static shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const randomPosition = Math.floor(Math.random() * i);
+
+      [array[i], array[randomPosition]] = [array[randomPosition], array[i]];
+    }
+
+    return array;
+  }
+}
+
+module.exports = {
+  Utils
+};
