2cc30e134b5fc979841b97e1c0a05d629b09c03d
Loktev Alexey <arloktev@mail.ru>
2022-01-09 22:23:30 +0300

    feat(templates): добавил шаблоны [WIP]

--- src/express/express.js
+++ src/express/express.js
@@ -2,11 +2,25 @@
 
 const express = require(`express`);
 const router = require(`./router`);
+const {HttpCode} = require(`../constants`);
+const path = require(`path`);
 
 const DEFAULT_PORT = 8080;
+const PUBLIC_DIR = `public`;
+const VIEWS_DIR = `templates`;
 
 const app = express();
 
 app.use(router);
+
+app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
+
+app.use((req, res) => res.status(HttpCode.NOT_FOUND).render(`errors/404`));
+app.use((err, req, res, next) => {
+  return res.status(HttpCode.INTERNAL_SERVER_ERROR).render(`errors/500`);
+});
+
+app.set(`views`, path.resolve(__dirname, VIEWS_DIR));
+app.set(`view engine`, `pug`);
 
 app.listen(DEFAULT_PORT);
