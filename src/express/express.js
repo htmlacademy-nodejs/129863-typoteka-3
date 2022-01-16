906f12d37297a0840a64395411ebfcef022803f6
Loktev Alexey <arloktev@mail.ru>
2022-01-16 10:57:56 +0300

    правки после кодревью refactor(router): вынес роутер в отдельный модуль fix(articlesRoutes): скорректировал вывод динамического параметра feat(scripts): добавил запуск express в двух режимах

--- src/express/express.js
+++ src/express/express.js
@@ -1,17 +1,12 @@
 'use strict';
 
 const express = require(`express`);
-
-const mainRoutes = require(`./routes/main-routes`);
-const myRoutes = require(`./routes/my-routes`);
-const articlesRoutes = require(`./routes/articles-routes`);
+const router = require(`./router`);
 
 const DEFAULT_PORT = 8080;
 
 const app = express();
 
-app.use(`/`, mainRoutes);
-app.use(`/my`, myRoutes);
-app.use(`/articles`, articlesRoutes);
+app.use(router);
 
 app.listen(DEFAULT_PORT);
--- src/express/routes/articles-routes.js
+++ src/express/routes/articles-routes.js
@@ -4,9 +4,9 @@ const {Router} = require(`express`);
 
 const articlesRoutes = new Router();
 
-articlesRoutes.get(`/category/:id`, (req, res) => res.send(`/articles/category/:id`));
+articlesRoutes.get(`/category/:id`, (req, res) => res.send(`/articles/category/${req.params.id}`));
 articlesRoutes.get(`/add`, (req, res) => res.send(`/articles/add`));
-articlesRoutes.get(`/edit/:id`, (req, res) => res.send(`/articles/edit/:id`));
-articlesRoutes.get(`/:id`, (req, res) => res.send(`/articles/:id`));
+articlesRoutes.get(`/edit/:id`, (req, res) => res.send(`/articles/edit/${req.params.id}`));
+articlesRoutes.get(`/:id`, (req, res) => res.send(`/articles/${req.params.id}`));
 
 module.exports = articlesRoutes;
